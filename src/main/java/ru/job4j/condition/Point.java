a2c69596a1d9b0c1d904d1dcc8ecd27d65126a28
@npanin <npanin@gid.ru>
2024-03-23 22:09:19 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587 #28123]

---
+++ src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,37 @@
+package ru.job4j.condition;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+public class Point {
+
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int x;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double distance = a.distance(b);
+        System.out.println(distance);
+    }
+
+    public double distance(int x1, int y1, int x2, int y2) {
+        return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    }
+}
---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PointTest {
+
+    @Test()
+    public void whenThisPoint0And0AndThatPoint0And2ThenDistanceEqual2And0() {
+        Point x = new Point(0, 0);
+        Point y = new Point(0, 2);
+        double expected = 2.0;
+        assertThat(x.distance(y)).isEqualTo(expected);
+    }
+
+    @Test()
+    public void whenThisPoint0And0AndThatPoint0And0ThenDistanceEqual0And0() {
+        Point x = new Point(0, 0);
+        Point y = new Point(0, 0);
+        double expected = 0.0;
+        assertThat(x.distance(y)).isEqualTo(expected);
+    }
+}
