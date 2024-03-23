4f117613e5fdf6c4c3f0f2af74d06f1556978d0a
@npanin <npanin@gid.ru>
2024-03-23 22:31:08 +0300

    2.1. Рефакторинг - Площадь треугольника. [#122642 #28132]

---
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+import org.assertj.core.api.AbstractDoubleAssert;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
+
+public class TriangleTest {
+    @Test
+    public void when00and40and04Then8() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = 8;
+        assertThat(result).isCloseTo(expected, offset(0.001));
+    } 
+    @Test
+    public void when00and00and00ThenFalse() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 0);
+        Point c = new Point(0, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double ab = a.distance(b);
+        double ac = a.distance(c);
+        double bc = b.distance(c);
+        boolean result = triangle.exist(ab, ac, bc);
+        boolean expected = false;
+        assertThat(result).isEqualTo(expected);
+    }
+}
